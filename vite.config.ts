f701d5b241b348739d5544c2aabe9a6fd32d8e58
eduulmer <76127601+eduulmer@users.noreply.github.com>
2026-02-20 09:00:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/praia-guide-sc/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
